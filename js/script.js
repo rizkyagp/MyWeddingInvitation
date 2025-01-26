e0127abd7fd1108ef53e1b7022342f23279266da
rizkyagp <129676339+rizkyagp@users.noreply.github.com>
2025-01-26 20:52:50 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 // Step 1: Get the URL parameter
 const urlParams = new URLSearchParams(window.location.search);
-const toValue = urlParams.get('to'); // Get the value of 'to'
+const toValue = urlParams.get("to"); // Get the value of 'to'
 
 // Step 2: Display the parameter on the page
 if (toValue) {
-    document.getElementById('invitation-message').textContent = `Kepada Yth. ${toValue}, kami mengundangmu di hari spesial kami.`;
+  document.getElementById(
+    "invitation-message"
+  ).textContent = `Kepada Yth. ${toValue}, kami mengundangmu di hari spesial kami.`;
 } else {
-    document.getElementById('invitation-message').textContent = 'Tidak ada nama yang ditemukan.';
+  document.getElementById("invitation-message").textContent =
+    "Tidak ada nama yang ditemukan.";
 }
 
-document.getElementById('open-invitation').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'none'; // Hide the overlay
-});
+document
+  .getElementById("open-invitation")
+  .addEventListener("click", function () {
+    document.getElementById("overlay").style.display = "none"; // Hide the overlay
+
+    var audio = document.getElementById("myAudio");
+    audio.play();
+
+    var playVideo = document.getElementById("myVideo");
+    myVideo.play();
+  });
